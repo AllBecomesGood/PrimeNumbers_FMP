4f782cb733a07eba8e9d2de1199c41c3581bfac8
AllBecomesGood <paschnell@dundee.ac.uk>
2017-03-04 19:26:30 +0000

    Started Prime computation.
    Started Sieve of Eratosthenes implementation. Estimating value of the
    highest Prime that the input will result in so that Sieve can work.

--- Prime.java
+++ Prime.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-
+import static java.lang.Math.log;
 /**
 Search file for TODO:
 - input validation
+- discard even numbers except 2
 
 */
 public class Prime
@@ -28,6 +29,7 @@ public class Prime
 	{
 		int input = getUserInput();
 		System.out.println("coord:You entered: " + input);
+		sieveOfEratosthenes(input);
 	}
 	/**
 	 * Method to get user input (natural number > 0).
@@ -47,6 +49,29 @@ public class Prime
 		}
 		System.out.println("You entered: " + input);
 		return input;
+	 }
+	 /**
+	 * Method to compute Prime numbers. 
+	 * - TODO: exclude any even numbers except 2 right away
+	 *
+	 * @param
+	 * @return
+	 */
+	 private void sieveOfEratosthenes(int input)
+	 {
+		 // Have to estimate the highest prime of the input.
+		 // http://stackoverflow.com/questions/1042717/is-there-a-way-to-find-the-approximate-value-of-the-nth-prime/1069023#1069023
+		 double in = (double)input;
+		 double estimate = in*log(in) + in*log(log(in));
+		 int estimateHighestPrime = (int)Math.ceil(estimate);
+		 System.out.println("Estimate of highest Prime: " + estimateHighestPrime);
+		 System.out.println("1mil and 1th prime is 15,485,867");
+		 System.out.println("2mil and 1th prime is 32,452,867");
+		 System.out.println("2, 3, 5, 7, 11, 13, 17, 19"); //expecting wrong estimate for low numbers
+		 // When input is 3, the estimate is too low.
+
+
+		 
 	 }
 	
 }
